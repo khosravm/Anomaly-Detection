e7b74c6d7a89d30ce0c03d44a0451cbeaffadf53
khosravm <55493282+khosravm@users.noreply.github.com>
2021-09-15 18:41:11 +0200

    Update DownLoad_Dataset.py

--- Recurrent neural networks for AD/DownLoad_Dataset.py
+++ Recurrent neural networks for AD/DownLoad_Dataset.py
@@ -10,13 +10,6 @@ urls['ecg']=['http://www.cs.ucr.edu/~eamonn/discords/ECG_data.zip',
              'http://www.cs.ucr.edu/~eamonn/discords/qtdbsele0606.txt',
              'http://www.cs.ucr.edu/~eamonn/discords/chfdbchf15.txt',
              'http://www.cs.ucr.edu/~eamonn/discords/qtdbsel102.txt']
-urls['gesture']=['http://www.cs.ucr.edu/~eamonn/discords/ann_gun_CentroidA']
-urls['space_shuttle']=['http://www.cs.ucr.edu/~eamonn/discords/TEK16.txt',
-                       'http://www.cs.ucr.edu/~eamonn/discords/TEK17.txt',
-                       'http://www.cs.ucr.edu/~eamonn/discords/TEK14.txt']
-urls['respiration']=['http://www.cs.ucr.edu/~eamonn/discords/nprs44.txt',
-                     'http://www.cs.ucr.edu/~eamonn/discords/nprs43.txt']
-urls['power_demand']=['http://www.cs.ucr.edu/~eamonn/discords/power_data.txt']
 
 for dataname in urls:
     raw_dir = Path('dataset', dataname, 'raw')
